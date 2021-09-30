afd135200a301dff86b6380bd7178732308e4416
aaronkaplan <aaron@lo-res.org>
2021-09-30 11:18:49 +0200

    Add test cases for unit tests

---
+++ tests/test_cache.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from lib.cache import Cache
+
+class TestCache(TestCase):
+    c = None
+
+    def __init__(self):
+        self.c = Cache()
+
+    def test__setitem__getitem__(self):
+        self.c["foo"] = "bar"
+        assert "bar" == self.c["foo"]
+        self.c['complex_dict'] = json.dumps({'bobo': 'baba', 'barbarella': 68})
+        print("complex dict = %r" % json.loads(self.c['complex_dict']))
+
+    def test__contains__(self):
+        self.c["foo"] = "bar"
+        self.c['foo44'] = "xyz"
+        assert "foo" in self.c
+        assert "foo44" in self.c
+
+    def test__len__(self):
+        print("number of entries in the cache dict: %d" % len(self.c))
+        assert len(self.c) == 4
+
