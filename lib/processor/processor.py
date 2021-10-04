ae64a4ef74047bf229c40bf36ab2c2d4931afd85
aaronkaplan <aaron@lo-res.org>
2021-10-04 14:42:58 +0200

    initial skeleton for a MyProducer

--- lib/processor/processor.py
+++ lib/processor/processor.py
@@ -100,7 +100,7 @@ class MyProcessor(AbstractProcessor):
         
     def process(self, ch = None, method = None, properties = None, msg: dict = {}):
         
-        logging.debug("MyProcessor (ID: %s). Got msg %r" % (self.id, msg))
+        logging.info("MyProcessor (ID: %s). Got msg %r" % (self.id, msg))
         self.msg = msg
         
     def run(self):
