66cee542220c813cc5fccc13f91f7310ea86336c
aaronkaplan <aaron@lo-res.org>
2021-10-05 13:58:27 +0200

    add sample workflow yaml + basic parsing for it.

---
+++ lib/workflow.py
@@ -0,0 +1,22 @@
+"""Workflow generator. This python module reads in a workflow.yml config file and will instantiate all required
+processors and connect them to the right queues and exchanges."""
+
+import yaml
+
+from config import config
+
+
+DEFAULT_WORKFLOW_FILE="etc/workflow.yml"
+
+
+class Workflow():
+    workflow_config = ""
+
+    def __init__(self, workflow_config: str = DEFAULT_WORKFLOW_FILE):
+        with open(workflow_config, 'r') as f:
+            wf = yaml.safe_load(f)            
+            print(wf)
+
+
+if __name__ == "__main__":
+    wf = Workflow()
