3da59759a2975cc9296c2a146a601775038470c9
aaronkaplan <aaron@lo-res.org>
2021-10-15 00:16:28 +0200

    formatting

--- processors/enrichers/gethostbyname/gethostbyname.py
+++ processors/enrichers/gethostbyname/gethostbyname.py
@@ -1,7 +1,8 @@
-from lib.processor.enricher import Enricher
+import logging
 import socket
 import uuid
-import logging
+from lib.processor.enricher import Enricher
+
 
 # see https://stackoverflow.com/questions/2805231/how-can-i-do-dns-lookups-in-python-including-referring-to-etc-hosts
 # thanks stackoverflow
@@ -21,7 +22,7 @@ def get_ips_by_dns_lookup(fqdn, port=None):
     """
 
     try:
-        l = list(map(lambda x: x[4][0], socket.getaddrinfo(fqdn, port, proto=socket.IPPROTO_TCP)))
+        l = list(map(lambda x: x[4][0], socket.getaddrinfo(fqdn, port, proto = socket.IPPROTO_TCP)))
     except Exception as ex:
         logging.info("Could not resolve domain %s. Reason: %s" % (fqdn, str(ex)))
         return []
@@ -32,7 +33,7 @@ class GetHostByName(Enricher):
     """A very simple / KISS gethostbyname enricher. It will fetch the "fqdn" key-value pair from the message and add a list of IPs to the message """
     dns_recursor = "8.8.8.8"
 
-    def process(self, channel = None, method = None, properties = None, msg: dict = {}):
+    def process(self, channel=None, method=None, properties=None, msg: dict = {}):
         fqdn = msg.get('fqdn', None)
         if fqdn:
             ips = get_ips_by_dns_lookup(fqdn)
@@ -45,7 +46,7 @@ if __name__ == "__main__":
     print(ips)
     assert '93.184.216.34' in ips
 
-    e = GetHostByName(id="sample-gethostbyname")
+    e = GetHostByName(id = "sample-gethostbyname")
     newmsg = e.process("ch1", msg = {"fqdn": "example.com"})
     print(newmsg)
     assert 'ips' in newmsg.keys() and '93.184.216.34' in newmsg['ips']
