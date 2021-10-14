651b07f49fa19c33c8d6b9301466b183bb781a10
aaronkaplan <aaron@lo-res.org>
2021-10-14 23:25:06 +0200

    make sure that non-resolving fqdns return an empty list of IPs + test it

--- processors/enrichers/gethostbyname/gethostbyname.py
+++ processors/enrichers/gethostbyname/gethostbyname.py
@@ -1,24 +1,31 @@
 from lib.processor.enricher import Enricher
 import socket
-
+import uuid
+import logging
 
 # see https://stackoverflow.com/questions/2805231/how-can-i-do-dns-lookups-in-python-including-referring-to-etc-hosts
 # thanks stackoverflow
 
-def get_ips_by_dns_lookup(target, port=443):
+def get_ips_by_dns_lookup(fqdn, port=443):
     '''
-        this function takes the passed target and optional port and does a dns
+        this function takes the passed fqdn and optional port and does a dns
         lookup. it returns the ips that it finds to the caller.
 
-        :param target:  the URI that you'd like to get the ip address(es) for
-        :type target:   string
+        :param fqdn:    the URI that you'd like to get the ip address(es) for
+        :type fqdn:     string
         :param port:    which port do you want to do the lookup against?
         :type port:     integer
-        :returns ips:   all of the discovered ips for the target
+        :returns ips:   all of the discovered ips for the fqdn
         :rtype ips:     list of strings
 
     '''
-    return list(map(lambda x: x[4][0], socket.getaddrinfo('{}.'.format(target),port,type=socket.SOCK_STREAM)))
+    try:
+        l =  list(map(lambda x: x[4][0], socket.getaddrinfo('{}.'.format(fqdn), port, type=socket.SOCK_STREAM)))
+    except Exception as ex:
+        logging.info("Could not resolve domain %s. Reason: %s" %(fqdn, str(ex)))
+        return []
+    return l
+
 
 
 class GetHostByName(Enricher):
@@ -34,7 +41,7 @@ class GetHostByName(Enricher):
 
 
 if __name__ == "__main__":
-    ips = get_ips_by_dns_lookup(target='example.com')
+    ips = get_ips_by_dns_lookup(fqdn = 'example.com')
     print(ips)
     assert '93.184.216.34' in ips
 
@@ -42,3 +49,6 @@ if __name__ == "__main__":
     newmsg = e.process("ch1", msg = { "fqdn": "example.com" } )
     print(newmsg)
     assert 'ips' in newmsg.keys() and '93.184.216.34' in newmsg['ips']
+    # negative test case: there is no ip for this domain
+    newmsg = e.process("ch1", msg = { "fqdn": "{}.com".format(uuid.uuid4())})
+    assert 'ips' in newmsg.keys() and len(newmsg['ips']) == 0
