a83afb87a5693afc512b6c2994b9fbef3eaa8220
giogada <dan-stefan.gioga@ext.ec.europa.eu>
2021-10-18 11:21:08 +0200

    Implements __getitem__ method for Config Class Implements default path if none is specified for Config class load method Implements global config object instantiation using projectutils path methods

--- lib/config.py
+++ lib/config.py
@@ -13,6 +13,7 @@ import os
 import logging
 import yaml
 from pathlib import Path
+from lib.utils.projectutils import ProjectUtils
 
 
 __all__ = ["config", "Config"]
@@ -27,17 +28,33 @@ class Config:
 
     def load(self, file: Path):
         """Load the config file."""
+        # if no path is supplied get the config from the default location inside the project folder
+        if file == None:
+            p = ProjectUtils()
+            file = Path(p.get_config_path_as_str())
         try:
             with open(file, 'r') as _f:
                 self.params = yaml.safe_load(_f)
-        except Exception as ex:
+        except (OSError,FileNotFoundError) as ex:
             print("could not load config file %s. Reason: %s" % (file, str(ex)))
         return self.params
 
+    def __getitem__(self, item):
+        if item in self.params.keys():
+            return self.params[item]
+        else:
+            return None
 
-ROOTDIR = os.getenv('ROOTDIR', '/')
 
+# TODO:     Implement as standalone class and instantiate wherever necessary, idealy through projectutils and
+#           env varibales rather than instantiating globally. Will probably need to implement all the interfaces
+#           that dict implements at that point to support dict like interaction.
+#if __name__ == "__main__":
+p = ProjectUtils()
+ROOTDIR_str = p.get_project_path_as_str()
+config_file_path_str = p.get_config_path_as_str()
 _c = Config()
-config = _c.load(Path(ROOTDIR) / 'etc' / "config.yml")
-config['general']['ROOTDIR'] = ROOTDIR
+config = _c.load(Path(config_file_path_str))
+
+#config['general']['ROOTDIR'] = ROOTDIR
 logging.info(config)
