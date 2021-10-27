a35114dd234c73670eaba5a09e5178da8fb402f3
aaronkaplan <aaron@lo-res.org>
2021-10-27 11:31:16 +0200

    mock a function call

--- processors/enrichers/es_hunter/es_hunter.py
+++ processors/enrichers/es_hunter/es_hunter.py
@@ -14,7 +14,7 @@ def elastic_hunter_lookup_hash(hash: str) -> bool:
     """
 
     try:
-        data = es_conn.lookup(hash)
+        data = "Foobar"     # es_conn.lookup(hash)
     except Exception as ex:
         logging.info("Could not look up data in ES Hunter (key: %s). Reason: %s" % (hash, str(ex)))
         return False
