8aeba1d106b383d493abe5b3a18f811baafe3e8c
AaronK <aaronkaplan@users.noreply.github.com>
2021-11-05 10:05:23 +0100

    update the unit tests to our experiment

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
 from pathlib import Path
 from lib.config import Config, ROOTDIR
-
+from lib.utils.projectutils import ProjectUtils
 
 class TestConfig(TestCase):
 
     def test_load(self):
         _c = Config()
-        config = _c.load(Path(ROOTDIR) / 'etc' / "config.yml")
+        pu = ProjectUtils()
+        config = _c.load(pu.get_config_path_as_str())
         assert 'general' in config
         assert 'mq' in config['general']
 
