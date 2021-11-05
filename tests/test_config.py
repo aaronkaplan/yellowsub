764ad04683f43903a62abc9a05b544d636264fcd
aaronkaplan <aaron@lo-res.org>
2021-11-05 15:41:03 +0100

    create unit tests for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,9 +5,29 @@ from lib.config import Config, ROOTDIR
 
 class TestConfig(TestCase):
 
-    def test_load(self):
+    def load_testcases_config(self):
         _c = Config()
-        config = _c.load(Path(ROOTDIR) / 'etc' / "config.yml")
+        return _c.load(Path(ROOTDIR) / 'tests/data/etc' / "config.yml")
+
+    def test_load(self):
+        config = self.load_testcases_config()
+        assert isinstance(config, dict)
         assert 'general' in config
         assert 'mq' in config['general']
+
+    def test_store(self):
+        config = self.load_testcases_config()
+        with pytest.raises(RuntimeError):
+            config.store('/tmp/foo')
+
+    def test___setitem__getitem__(self):
+        config = self.load_testcases_config()
+        assert 'mq' in config['general']
+        config['foobar'] = "xyz"
+        assert config['foobar'] == "xyz"
+
+    def test___len__(self):
+        config = self.load_testcases_config()
+        assert len(config) == 4
+
 
