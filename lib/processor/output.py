988ff7561867e585b7c3fff4c4e90dc9b24d0866
Aaron Kaplan <aaron@lo-res.org>
2021-11-30 11:31:21 +0100

    output buts ofc need an id as well.

--- lib/processor/output.py
+++ lib/processor/output.py
@@ -4,5 +4,5 @@ from lib.processor.processor import Processor
 
 
 class OutputProcessor(Processor):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id: str, n: int = 1):
+        super().__init__(id, n)
